e2a22412132b49991d4263cec10c610632060416
rrozdoce <felipefelipevilhalva@gmail.com>
2024-09-12 07:42:31 -0400

    .

---
+++ af_er.py
@@ -0,0 +1,14 @@
+# Trabalho 3 de LFA
+# Alunos: 
+# Felipe Echeverria Vilhalva RGM: 45611
+# Bruno                      RGM:
+
+class Automato:
+    def __init__(self) -> None:
+        ()
+    
+    def ler_automato(self) -> None:
+        ()
+        
+    def converter_automato(self) -> None:
+        ()
