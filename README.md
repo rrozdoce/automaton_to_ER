9f6cd689c155f8ef5ed14b3a077f92f7baccfdeb
rrozdoce <felipefelipevilhalva@gmail.com>
2024-09-12 08:04:54 -0400

    .

--- README.md
+++ README.md
@@ -4,3 +4,8 @@ AF => ER
 Entrada: AF(AFN ou AFD ou AFNe)
 Saida: ER
 Data: 16/09/2024
+
+:-
+IDA
+OllyDbg --> clone do Evans Debugger
+Gdb
--- af_er.py
+++ af_er.py
@@ -7,8 +7,14 @@ class Automato:
     def __init__(self) -> None:
         ()
     
+    # ler o automato do usuario    
     def ler_automato(self) -> None:
         ()
-        
-    def converter_automato(self) -> None:
+    
+    # verificar se o automato é afd, afn ou afne, se caso for afn ou afne, realizar a conversao para afd
+    def verificar_automato() -> None:
+        ()
+    
+    # converter automato para expressão regular
+    def converter_ER(self) -> None:
         ()
